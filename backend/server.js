7da5ec29263ef4ce53fda254ad3b99de59577171
Md Shoaib <mdshoaib@Mds-MacBook-Air.local>
2025-09-01 14:10:35 +0530

    Backend initial commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ import createAdmin from "./utils/createAdmin.js";
 // -------------------- Env Config --------------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config();
 
 const app = express();
 
